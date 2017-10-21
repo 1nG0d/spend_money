dcc16fe08fcc14614ffdf584e2b204f70d334c7e
Qunkero <dron.andrew2011@yandex.ua>
2017-10-21 21:56:55 +0300

    material-ui

--- frontend/App.js
+++ frontend/App.js
@@ -1,15 +1,13 @@
 import React, {Component} from 'react'
 import Root from './components/Root'
-import store from './redux'
+// import store from './redux'
 import {Provider} from 'react-redux'
 import './config'
 
 class App extends Component {
     render() {
         return (
-            <div>
-
-            </div>
+            <Root/>
         )
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,38 @@
 module.exports = {
     context: __dirname + '/frontend',
-    entry:   {
+    entry: {
         index: ['./index']
     },
     devtool: 'inline-source-map',
-    output:  {
-        path:       __dirname + '/public/script',
+    output: {
+        path: __dirname + '/public/script',
         publicPath: '/script',
-        filename:   '[name].js'
+        filename: '[name].js'
     },
 
     module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['es2015','react', 'stage-0']
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react', 'stage-0']
+                }
+            },
+            {
+                rules: [
+                    {
+                        test: /\.css$/,
+                        use: ['style-loader', 'css-loader']
+                    }
+                ]
             }
-        }]
+        ]
     },
     devServer: {
         // hot:true,
-        contentBase:__dirname + '/public',
+        contentBase: __dirname + '/public',
         host: 'localhost', // default
         port: 3000, // default
         historyApiFallback: true
